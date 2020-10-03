26fe4a574bec79a8181b8bd093eefa8f55549842
d-jo <declan.l.johnson@gmail.com>
2020-10-03 14:21:59 -0500

    Remove react depend

---
+++ backend/static/js/postits.js
@@ -0,0 +1,53 @@
+
+function submitPostit() {
+  auth = $("#author").val();
+  content = $("#content").val();
+  dat = {
+    "Author": auth,
+    "Content": content
+  }
+  $.ajax({
+    type: "POST",
+    url: "/insert",
+    data: JSON.stringify(dat),
+    success: (data) => {
+      loadNotes();
+    },
+    error: (err) => {
+      loadNotes();
+    },
+    dataType: "json"
+  })
+}
+
+function loadNotes() {
+  $(".postits").empty();
+  $.ajax({
+    type: "GET",
+    url: "/latest",
+    success: (data) => {
+      console.log("Recieved data from server")
+      console.log(data)
+      for (let i = 0; i < data.length; i++) {
+        console.log(data[i])
+        let wrap = $("<div class='postit-container'></div>")  
+        let head = $("<div class='postit-header'></div>")
+        let cont = $("<div class='postit-content'></div>").text(data[i]["Content"])
+        let ele = $("<h3></h3>").text("[" + data[i]['ID'] + "] " + data[i]["Author"])
+        head = head.append(ele)
+        wrap = wrap.append(head, cont)
+        $(".postits").append(wrap)
+      }
+    },
+    error: (err) => {
+      console.log('err')
+      console.log(err)
+    },
+    dataType: "json"
+  })
+}
+
+$(() => {
+  console.log("Document ready");
+  loadNotes()
+})
